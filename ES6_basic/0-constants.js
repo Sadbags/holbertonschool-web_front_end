3e87e46643d2eadcf2be55c15c2e7d8d9bebdfa6
Bryan  Garcia <98924720+Sadbags@users.noreply.github.com>
2024-07-16 12:15:07 -0400

    Update 0-constants.js

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
@@ -15,4 +13,3 @@ export function taskNext() {
 
   return combination;
 }
->>>>>>> 5dd4cdf0cad3b495d2dc3dcceeb1cc42c5d4a482
