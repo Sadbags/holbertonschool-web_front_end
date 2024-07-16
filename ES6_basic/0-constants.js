5dd4cdf0cad3b495d2dc3dcceeb1cc42c5d4a482
Bryan  Garcia <98924720+Sadbags@users.noreply.github.com>
2024-07-16 11:57:34 -0400

    Update 0-constants.js

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,11 +1,6 @@
 export function taskFirst() {
-<<<<<<< HEAD
-	const task = 'I prefer const when I can.';
-	return task;
-=======
   const task = 'I prefer const when I can.';
   return task;
->>>>>>> 54641f8b4c0119e0659860e234409088ebea0aaf
 }
 
 export function getLast() {
@@ -16,9 +11,5 @@ export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
 
-<<<<<<< HEAD
-	return combination;
-=======
   return combination;
->>>>>>> 54641f8b4c0119e0659860e234409088ebea0aaf
 }
